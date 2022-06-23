2c67a4a8620346795d0441ddd5efa40bd2a186d7
mr-abdellah <belkaid.abdulah@gmail.com>
2022-06-23 06:06:16 +0100

    make the code dry

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 // import Css ,images sources =>
 import './style.css';
-import refresh from './img/refresh.png';
-import returny from './img/return.png';
-import save from './modules/saveTasks.js';
-import displayTask from './modules/displayTask.js';
 
-const refreshImg = document.querySelector('#refresh');
-const returnImg = document.querySelector('#return');
+import Tasks from './modules/tasksClass.js';
+import TodoDOM from './modules/create.js';
+import SaveTasks from './modules/saveTasks.js';
 
-refreshImg.src = refresh;
-returnImg.src = returny;
+const todoStore = new SaveTasks();
+const todoList = new Tasks(todoStore.items);
+const todoDOM = new TodoDOM();
 
-save();
-displayTask();
+todoDOM.onCheckBoxChange = (event) => {
+  const index = parseInt(event.target.dataset.index, 10);
+  const { checked } = event.currentTarget;
 
-const refreshBtn = document.querySelector('#btnRefresh');
-refreshBtn.addEventListener('click', () => {
-  window.location.reload();
+  todoList.completed(index, checked);
+  todoStore.completed(index, checked);
+};
+
+todoDOM.onInputChange = (event) => {
+  const index = parseInt(event.target.dataset.index, 10);
+  const newTodo = event.target.value;
+
+  todoList.edit(index, newTodo);
+  todoStore.edit(index, newTodo);
+};
+
+todoDOM.onRemoveItemClick = (event) => {
+  const index = parseInt(event.currentTarget.dataset.index, 10);
+
+  todoList.delete(index);
+  todoStore.delete(index);
+  todoDOM.remove(index);
+};
+
+todoDOM.render(todoList.items);
+
+todoDOM.on('enter', (todo) => {
+  const newTodo = todoList.add(todo);
+  todoStore.add(newTodo);
+  todoDOM.add(newTodo);
+});
+
+todoDOM.on('clear-all', (indexes) => {
+  todoList.clearAll(indexes);
+  todoStore.store(todoList.items);
+  todoDOM.render(todoList.items);
 });
